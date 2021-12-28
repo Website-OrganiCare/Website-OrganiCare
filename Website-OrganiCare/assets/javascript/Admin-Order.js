47784a59882670b81bd75051ff81e7b4c8eae001
Quan Nguyen <walker30122003@gmail.com>
2021-12-28 14:00:04 +0700

    Just a commit

--- Website-OrganiCare/assets/javascript/Admin-Order.js
+++ Website-OrganiCare/assets/javascript/Admin-Order.js
@@ -1,14 +1,13 @@
-﻿var acc = document.getElementsByClassName("admin-accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
+﻿function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("order-tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "grid";
+    evt.currentTarget.className += " active";
 }
